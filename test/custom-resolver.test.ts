0e1fb96d23340bb2c5e2587bedbd411ca4ac6636
JiangWeixian <jiangweixian1994@gmail.com>
2022-07-23 12:24:17 +0800

    :white_check_mark:  update snapshot

--- test/custom-resolver.test.ts
+++ test/custom-resolver.test.ts
@@ -1,25 +1,23 @@
 import { PageContext } from 'vite-plugin-pages'
 import path from 'path'
-import { describe, it } from 'vitest'
+import { describe, it, expect } from 'vitest'
 
-import { reactResolver } from '../src/resolver'
+import { nextEnhancedResolver } from '../src/resolver'
 
 const fixtures = (...args: string[]) => path.resolve(__dirname, './fixtures', ...args)
 
 describe('custom-resolver', () => {
-  it('TODO', async () => {
+  it('next enhanced resolver', async () => {
     const page = new PageContext(
       {
         importMode: 'sync',
-        routeStyle: 'remix',
-        resolver: reactResolver() as any,
+        resolver: nextEnhancedResolver() as any,
         dirs: ['react'],
       },
       fixtures(),
     )
     await page.searchGlob()
     const routes = await page.resolveRoutes()
-    console.log(routes)
     expect(routes).toMatchSnapshot()
   })
 })

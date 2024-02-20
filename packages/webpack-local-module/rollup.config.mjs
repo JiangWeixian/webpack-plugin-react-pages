e152d936e6455e8ef00cced29cd49cd436388261
JW <jiangweixian1994@gmail.com>
2024-02-20 17:25:47 +0800

    chore: package.json config (#21)
    * :wrench:  bundle target node16
    
    * :wrench:  external fs-extra and ufo

--- packages/webpack-local-module/rollup.config.mjs
+++ packages/webpack-local-module/rollup.config.mjs
@@ -27,7 +27,7 @@ export default defineConfig([
       }),
       commonjs(),
       esbuild({
-        target: 'node14',
+        target: 'node16',
       }),
       alias({
         customResolver: resolve({ extensions: ['.tsx', '.ts'] }),

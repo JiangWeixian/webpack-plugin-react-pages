c02e20bc8cfad342aa487c346a5b33a2107daaf7
JW <jiangweixian1994@gmail.com>
2024-04-14 17:27:49 +0800

    :zap:  virtual:react-pages cacheable (#23)
    * :zap:  virtual:react-pages cacheable
    
    * :loud_sound:  add context dir

---
+++ .changeset/thirty-cows-marry.md
@@ -0,0 +1,5 @@
+---
+"webpack-plugin-react-pages": patch
+---
+
+`virtual:react-pages` should be cacheable
--- examples/rspack/rspack.config.js
+++ examples/rspack/rspack.config.js
@@ -14,6 +14,9 @@ module.exports = {
   entry: {
     main: './src/main.tsx',
   },
+  devServer: {
+    historyApiFallback: true
+  },
   resolve: {
     extensions: ['...', '.ts', '.tsx', '.jsx'],
   },
